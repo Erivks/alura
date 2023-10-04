f01da351e0c8c8aa08e0acf0a274e85e14a9bf97
Erick Oliveira <erick_santos00@outlook.com>
2023-10-04 16:14:16 -0300

    fix: Alterando nome de arquivo

--- design-pattern/src/CalculadoraDeImposto.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Src;
-
-class CalculadoraDeImpostos {
-    public function calcula(Orcamento $orcamento, Impostos\Imposto $imposto): Float {
-        return $imposto->calcula($orcamento);
-    }
-}
