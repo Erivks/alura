7c94395f6a39a9f1cd484941e6cb3cb7ea7f36b6
Erick Oliveira <erick_santos00@outlook.com>
2023-10-12 11:25:46 -0300

    feat: Adicionando Singleton

---
+++ design-pattern/src/PdoConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Src;
+
+class PdoConnection extends \PDO {
+    private static ?self $instance = null;
+
+    private function __construct($dsn, $username = null, $passwd = null, $options = null)
+    {
+        parent::__construct($dsn, $username, $passwd, $options);
+    }
+
+    public static function getInstance($dsn, $username = null, $passwd = null, $options = null): self {
+        if (is_null(self::$instance)) {
+            self::$instance = new static($dsn, $username, $passwd, $options);
+        }
+
+        return self::$instance;
+    }
+}
